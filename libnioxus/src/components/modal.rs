c2e06a17583291522962db8823e94346c87e85ac
nothendev <borodinov.ilya@gmail.com>
2023-06-28 15:42:58 +0300

    help

--- libnioxus/src/components/modal.rs
+++ libnioxus/src/components/modal.rs
@@ -3,18 +3,55 @@ use dioxus::prelude::*;
 #[derive(Clone, Debug, Props)]
 pub struct ModalProps<'a> {
     pub open: &'a bool,
+    pub header: Option<Element<'a>>,
     pub children: Element<'a>,
 }
 
-#[allow(non_snake_case)]
+#[derive(Clone, Debug, Props)]
+pub struct DefaultModalProps<'a> {
+    pub open: &'a UseState<bool>,
+    pub children: Element<'a>,
+}
+
+const DIALOG_STYLE: &str = include_str!("./modal.css");
+
+pub fn DefaultModal<'a>(cx: Scope<'a, DefaultModalProps<'a>>) -> Element<'a> {
+    let DefaultModalProps { open, children } = cx.props;
+    cx.render(rsx! {
+        Modal {
+            open: (*open).get(),
+            header: cx.render(rsx! {
+                div { style: "display: flex; justify-content: flex-end;",
+                    button { style: "font-size: 24px", onclick: move |_| (*open).set(false),
+                             "x"
+                    }
+                }
+            }),
+            children
+        }
+    })
+}
+
 pub fn Modal<'a>(cx: Scope<'a, ModalProps<'a>>) -> Element<'a> {
-    let ModalProps { open, children } = cx.props;
+    let ModalProps {
+        open,
+        header,
+        children,
+    } = cx.props;
 
     cx.render(rsx! {
         dialog {
             open: **open,
 
+            header.as_ref().map(|header| rsx! {
+                header
+                hr {}
+            })
+
             children
+        }
+        style {
+            DIALOG_STYLE
         }
     })
 }
--- libnioxus/src/lib.rs
+++ libnioxus/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(non_snake_case)]
 #![feature(decl_macro)]
 pub mod components;
 pub mod css;
